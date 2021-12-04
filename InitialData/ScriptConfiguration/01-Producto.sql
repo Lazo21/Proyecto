a5bb9e0c4b617302d058e6074c2c68535130c24d
KEVIN LAZO <klazo511@gmail.com>
2021-12-04 00:11:13 -0600

    Agregar archivos de proyecto.

---
+++ InitialData/ScriptConfiguration/01-Producto.sql
@@ -0,0 +1,42 @@
+﻿DROP TABLE IF EXISTS #ProductoTemp
+
+SELECT 
+IdProducto, Categoria, NombreProducto, Cantidad_Disponible, Caracteristicas, Estado INTO #ProductoTemp
+FROM (
+VALUES
+(1, ' ',' ',100,' ','DISPONIBLE'),
+(2, ' ',' ',20,' ','LIMITADO'),
+(3, ' ',' ',0,' ','AGOTADO')
+)AS TEMP (IdProducto, Categoria, NombreProducto, Cantidad_Disponible, Caracteristicas, Estado)
+
+----ACTUALIZAR DATOS---
+UPDATE P SET
+  P.Categoria= TM.Categoria,
+  P.NombreProducto= TM.NombreProducto,
+  P.Cantidad_Disponible= TM.Cantidad_Disponible,
+  P.Caracteristicas= TM.Caracteristicas,
+  P.Estado= TM.Estado
+FROM Dbo.Producto P
+INNER JOIN #ProductoTemp TM
+    ON P.IdProducto= TM.IdProducto  
+    
+    ----INSERTAR DATOS---
+
+SET IDENTITY_INSERT dbo.Producto ON
+
+INSERT INTO dbo.Producto(
+IdProducto,Categoria, NombreProducto, Cantidad_Disponible, Caracteristicas, Estado)
+SELECT
+IdProducto, Categoria, NombreProducto, Cantidad_Disponible, Caracteristicas, Estado
+FROM #ProductoTemp
+
+
+EXCEPT
+SELECT
+IdProducto, Categoria, NombreProducto, Cantidad_Disponible, Caracteristicas, Estado
+FROM dbo.Producto
+
+
+SET IDENTITY_INSERT dbo.Producto OFF
+
+GO
---
+++ ProyectoFinal/dbo/Tables/Producto.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Producto]
+(
+	[IdProducto] INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Producto PRIMARY KEY CLUSTERED(IdProducto)
+   ,Categoria VARCHAR(50) NOT NULL
+   ,NombreProducto VARCHAR(50) NOT NULL
+   ,Cantidad_Disponible INT NOT NULL
+   ,Caracteristicas VARCHAR(50) NOT NULL
+   ,Estado VARCHAR(50) NOT NULL
+)
