76ead71821fb4c63faa1b73e91aa1b92a02794db
KEVIN LAZO <klazo511@gmail.com>
2021-12-14 11:24:59 -0600

    Actualizacion BD, entity y WBL

---
+++ WebApp/Entity/ProductoEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class ProductoEntity
+    {
+        public int? IdProducto { get; set; }
+
+        public string Categoria { get; set; }
+       
+        public string NombreProducto { get; set; }
+
+        public int? Cantidad_Disponible { get; set; }
+
+        public string Caracteristicas { get; set; }
+
+        public string Estado { get; set; }
+
+    }
+}
