79c86d8f7252d19b259a5e65226fc78ec0ff5508
Lazo21 <klazo511@gmail.com>
2022-11-08 09:20:05 -0600

    cambios

--- secciones/reporte_contactos.php
+++ secciones/reporte_contactos.php
@@ -217,13 +217,13 @@
     <center><img src="../img/contactos.jpeg"/ width="1680" height="800" margin="auto" padding="600"></center>
     </div> -->
 
-    <?php
-    include 'secciones/plantilla.php';
+<?php
+    include ('secciones/plantilla.php');
     //require 'conexion.php';
 
-   // $query="SELECT e.estado, m.id_municipio, m.municipio FROM t_municipio AS m INNER JOIN 
-   // t_estado AS e ON m.id_estado=e.id_estado";
-   // $resultado = $mysqli->query($query);
+    $query="SELECT e.estado, m.id_municipio, m.municipio FROM t_municipio AS m INNER JOIN 
+    t_estado AS e ON m.id_estado=e.id_estado";
+    $resultado = $mysqli->query($query);
 
     $pdf = new PDF();
     $pdf->AliasNbPages();
@@ -240,8 +240,51 @@
     $pdf->Cell(50,6,'Teléfono Contacto Emergencia',1,1,'C',1);
 
     $pdf->Output();
+?>
+  
 
-   ?>
+
+
+//require('fpdf/fpdf.php');
+
+//class PDF extends FPDF
+//{
+// Cabecera de página
+//function Header()
+//{
+    // Logo
+    //$this->Image('logo.png',10,8,33);
+    // Arial bold 15
+   // $this->SetFont('Arial','B',15);
+    // Movernos a la derecha
+   // $this->Cell(80);
+    // Título
+    //$this->Cell(30,10,'Title',1,0,'C');
+    // Salto de línea
+    //$this->Ln(20);
+}
+
+// Pie de página
+//function Footer()
+//{
+    // Posición: a 1,5 cm del final
+  //  $this->SetY(-15);
+    // Arial italic 8
+   // $this->SetFont('Arial','I',8);
+    // Número de página
+    //$this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+//}
+//}
+
+// Creación del objeto de la clase heredada
+//$pdf = new PDF();
+//$pdf->AliasNbPages();
+//$pdf->AddPage();
+//$pdf->SetFont('Times','',12);
+//for($i=1;$i<=40;$i++)
+   // $pdf->Cell(0,10,'Imprimiendo línea número '.$i,0,1);
+//$pdf->Output();
+
 
 
 
