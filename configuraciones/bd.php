87e14d37adb11c1d2c9f401d70a6da897b604f89
Lazo21 <klazo511@gmail.com>
2022-11-11 17:25:37 -0600

    cambios echo

--- configuraciones/bd.php
+++ configuraciones/bd.php
@@ -9,7 +9,7 @@ class BD{
             if( !isset(self::$instancia) ){
                 $opciones[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
                 self::$instancia = new PDO('mysql:host=localhost;dbname=psicologia', 'root', '', $opciones);
-                echo"Conexion exitosa";
+                echo"Conexion exitosa...";
             }
             return self::$instancia;
 
