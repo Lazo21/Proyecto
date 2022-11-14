e71f92aab7a39a97b9ef3924ab5a90651e6d0223
Lazo21 <klazo511@gmail.com>
2022-11-14 05:53:32 -0600

    mostrar registros en tabla

--- secciones/vista_contactos.php
+++ secciones/vista_contactos.php
@@ -128,6 +128,7 @@
                 <tr>
                     <th scope="col" class="text-center"> Nombre </th>
                     <th scope="col" class="text-center"> Email </th>
+                    <th scope="col" class="text-center"> Email Secundario</th>
                     <th scope="col" class="text-center"> Teléfono </th>
                     <th scope="col" class="text-center"> Nombre (Contacto emergencia) </th>
                     <th scope="col" class="text-center"> Email (Contacto emergencia) </th>
@@ -136,18 +137,21 @@
                 </tr>
             </thead>
             <tbody>
-                <tr class="">
-                    <td>  </td>
-                    <td>  </td>
-                    <td>  </td>
-                    <td>  </td>
-                    <td>  </td>
-                    <td>  </td>                    
+               
+            <?php foreach($listaContactos as $contacto) { ?>
+                <tr>
+                    <td> <?php echo $contacto['Nombre']; ?> </td>
+                    <td> <?php echo $contacto['Email']; ?> </td>
+                    <td> <?php echo $contacto['Email2']; ?> </td>
+                    <td> <?php echo $contacto['Telefono']; ?> </td>
+                    <td> <?php echo $contacto['Nom_ce']; ?> </td>
+                    <td> <?php echo $contacto['Email_ce']; ?> </td>     
+                    <td> <?php echo $contacto['Tel_ce']; ?> </td>                
                     <td class="text-center"> <button type="button" class="btn btn-outline-primary">Seleccionar <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb-fill" viewBox="0 0 16 16">
                         <path d="M8.5 1.75v2.716l.047-.002c.312-.012.742-.016 1.051.046.28.056.543.18.738.288.273.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716.075.09.141.175.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0z"/>
                         </svg></button> </td>
                 </tr>
-
+            <?php } ?>
             </tbody>
         </table>
     </div>
