a504d2fafbb115b7ce6ae4b2807a15422a9eb376
Lazo21 <klazo511@gmail.com>
2022-11-14 19:15:32 -0600

    espacios

--- secciones/contactos.php
+++ secciones/contactos.php
@@ -3,13 +3,12 @@
 include_once ('../configuraciones/bd.php');
 $conexionBD=BD::crearInstancia();
 
-print_r($_POST);
 
 $consulta=$conexionBD->prepare("SELECT * FROM contactos");
 $consulta->execute();
 $listacontactos=$consulta->fetchAll();
 
-print_r($listacontactos);
+
 
 
 ?>
